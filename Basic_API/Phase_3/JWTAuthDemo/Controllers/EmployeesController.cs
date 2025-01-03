88db613a0088789a50efe6fc36b63c13f1fee6f3
Priyansh Khunt <58349765+KHUNTPRIYANSH@users.noreply.github.com>
2025-01-03 09:04:36 +0530

    push folder structure changes

---
+++ Basic_API/Phase_3/JWTAuthDemo/Controllers/EmployeesController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using JWTAuthDemo.Models;
+
+namespace JWTAuthDemo.Controllers
+{
+    [Authorize]
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            return Employee.GetEmployees();
+        }
+    }
+}
---
+++ Basic_API/Phase_3_Web_Api/CRUD_MYSQL_DB_WEB_API/App_Start/WebApiConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace CRUD_MYSQL_DB_WEB_API
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            
+            // Option 1 : Enable CORS globally
+            
+            var cors = new EnableCorsAttribute("*", "*", "*"); // Allow all origins, headers, and methods
+            config.EnableCors(cors);
+            // ex
+            // EnableCorsAttribute("https://somthing , https://other , .. ,. ", "*", "GET,POST,....")
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
